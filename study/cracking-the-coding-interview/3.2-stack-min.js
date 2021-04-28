595ac3adf0f752981f686f14f7eaf441832a3ea2
racross1 <racross1@gmail.com>
2021-04-28 10:12:20 -0700

    additional detail on solution

--- study/cracking-the-coding-interview/3.2-stack-min.js
+++ study/cracking-the-coding-interview/3.2-stack-min.js
@@ -1,53 +1,3 @@
-//JAVA IMPLEMENTATION
-//consists of creating a separate stack to keep track of the min values. 
-//from this video:
-'https://www.youtube.com/watch?v=OLQ_aWJIITw'
-  
-// public class StackMin {
-//     public static void main(String[] args) throws Exception {
-//         StackWithMin2 stack = new StackWithMin2();
-//         stack.push(1);
-//         stack.push(4);
-//         stack.push(5);
-//         stack.push(-1);
-//         stack.push(9);
-//         System.out.println(stack.min());
-//         stack.pop();
-//         stack.pop();
-//         System.out.println(stack.min());
-//     }
-  
-//     public class StackWithMin2 extends MyStack<Integer>{
-//       MyStack<Integer> s2;
-//       public StackWithMin2() {
-//         s2 = new MyStack<Integer>();
-//       }
-  
-//       public void push(int value) throws Exception {
-//         if(value <= min()) {
-//           s2.push(value);
-//         }
-//         super.push(value);
-//       }
-  
-//       public Integer pop() throws Exception {
-//         int value = super.pop();
-//         if(value == min()) {
-//           s2.pop();
-//         }
-//         return value;
-//       }
-  
-//       public int min() throws Exception {
-//         if(s2.isEmpty()) {
-//           return Integer.MAX_VALUE;
-//         } else {
-//           return s2.peek();
-//         }
-//       }
-  
-//     }
-
 
 
 //video explaining in JS
@@ -56,6 +6,9 @@
 
 
 // leetcode problem, my solution
+//see below that for additionally improved 2 stack solution
+//leetcode solutions section includes Javascript:
+'https://leetcode.com/problems/min-stack/solution/'
 
 /**
  * initialize your data structure here.
