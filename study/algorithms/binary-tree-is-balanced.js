fe70f08786eca91e8e612fcb18b28c9c610dbb34
racross1 <racross1@gmail.com>
2021-06-01 08:20:46 -0700

    more is balanced

--- study/algorithms/binary-tree-is-balanced.js
+++ study/algorithms/binary-tree-is-balanced.js
@@ -20,6 +20,106 @@
  */
  
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+
+//from this video
+'https://www.youtube.com/watch?v=_pP1UaL-Xi8'
+let isBalanced = function(root) {
+    if (root == null) {
+        return true;
+    }
+    
+    return getHeight(root) !== -1;
+};
+
+let getHeight = function(node) {
+    if (node == null) {
+        return 0;
+    }
+
+    //previously I was having a hard time understanding why this recursive function will start at the bottom of the tree
+    //I think it's because of the call STACK - such that for each successive node, it goes to the left and the right of the node to 
+    //get left and right. 
+    //it hits a null condition when the tree ends and so starts iterating from the bottom of the tree up, starting at the 
+    //last node on the stack (i.e. the one on the bottom)
+    let left = getHeight(node.left);
+    let right = getHeight(node.right);
+
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+        return -1;
+    }
+
+
+    //this here is the part currently giving me the most trouble. 
+    //for the current binary tree we get our height (i.e. max height between left and right) and add one to go up a level
+    //so going back up to the lines getting left and right
+    //we know that the height is the prev height plus1
+    //what I still don't understand is how this height gets allocated to the left or the right nodes so that we can compare them.
+    //i think it's foor the parent node - so it's the tallest point leading up to the parent, which is effectively the current tallest height at that node.
+    //then for the next node, we'll have the max height for the left vs the max height for the right, and that's where we check the difference
+    return Math.max(left, right) + 1;
+};
+
+
+
+//from this video
+'https://www.youtube.com/watch?v=yzXPqT_GBv8'
+
+ var isBalanced = function(root) {
+    //flag to show if is balanced or not
+    let flag = true
+
+    //run recursive height function starting at root
+    height(root);
+    return flag;
+
+    function height(node){
+        //if there is no node, return 0
+        //for root this means obvi if there is no root
+        if(!node)return 0;
+
+        //if flag has been triggered to be set to false
+        if(!flag) return;
+
+        //for first iteration, left is this function run for node left of root and node right of root
+        //so bringing in what I got to above, this will start at the bottom-most left node and the bottommost right
+        let left = height(node.left);
+        let right = height(node.right);
+
+        //check to make sure balance condition is still true. if the absolute difference between the left and the right 
+        //is greater than 1, change the flag to false
+        //check
+        if(Math.abs(left-right) > 1)flag = false
+
+        //max of height of left and height of right + 1
+        //so for a given node it is returning the max height between its left and right children
+        //the check condition gets broken if at any point the difference exceeds 1
+        return Math.max(left,right)+1
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
 //from leetcode discussion
 
 //  The idea - DFS O(N)
@@ -27,6 +127,8 @@
 //  If any subtree is not balanced, encrypt the information as Infinity
  var isBalanced = function(root) {
      
+    //this is similar to the 2 above - recurse starting at bottom and check / increment the height each time
+    //this one is a little more intuitive to me because each left and right node get their own incrementer 
      let dfs = function(node) {
          if (!node) return 0;
          let left = 1 + dfs(node.left);
@@ -39,6 +141,8 @@
  };
 //  The idea - DFS O(N^2)
 //  Standard Top-Down recursion
+
+//this one seems a little gnarlier - I think I prefer the above 3+
  var isBalanced = function(root) {
      if (!root) return true;
      
