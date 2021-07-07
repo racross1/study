044e25fc0d3f4926ff7d1a47e15291961eaff40c
racross1 <racross1@gmail.com>
2021-07-07 10:44:35 -0700

    2d mat start

--- study/algorithms/amazon/search-2D-matrix.js
+++ study/algorithms/amazon/search-2D-matrix.js
@@ -11,6 +11,39 @@
  * @param {number} target
  * @return {boolean}
  */
+
+
+//  Input: 
+ let matrix1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+ let target1 = 3
+//  Output: true
+
+
+[
+    [1, 3,  5, 7],
+    [10,11,16,20],
+    [23,30,34,60]
+]
+
+
+start[0][0]
+end[matrix.length - 1][matrix[0].length - 1]
+
+midRow =  startRow  - endRow / 2
+midCol = startCol - endCol / 2
+
  var searchMatrix = function(matrix, target) {
-    
+     //get midpoint
+     //process 2 separate subarrays
+
+     let rowStart = 0
+     let colStart = 0
+     let rowEnd = matrix.length - 1
+     let colEnd = matrix[0].length - 1
+
+     helper(rowStart, colStart, rowEnd, colEnd){
+         let mid = 
+     }
+
+     //helper(rowStart, colStart, rowEnd, colEnd)
 };
